61ff037506f71fdc377360cd36996ff850eaa668
CMS37 <rhtn037@naver.com>
2025-03-17 18:01:12 +0900

    비동기 API 호출 개선

--- tiktok.js
+++ tiktok.js
@@ -1,24 +1,9 @@
-const ExcelJS = require("exceljs");
-const path = require("path");
-
-// 엑셀 파일의 첫 번째 워크시트, 첫 번째 열 데이터를 배열로 읽어오는 함수
-async function loadExcelData(filePath) {
-	const workbook = new ExcelJS.Workbook();
-	await workbook.xlsx.readFile(filePath);
-	const worksheet = workbook.getWorksheet(1);
-	const data = [];
-	worksheet.eachRow({ includeEmpty: false }, (row) => {
-	  data.push(row.getCell(1).value);
-	});
-	return data;
-  }
-
-const testData = require("./TiktokTest.json");
+// const testData = require("./TiktokTest.json");
 
 async function saveTiktokData(client, selectedKeywords, selectedCountries, period, sorting, matchExactly) {
 	console.log("TikTok Data loading...");
 	
-	const Data = [];
+	const data = [];
 
 // 	for (const country of selectedCountries) {
 // 		for (const keyword of selectedKeywords) {
@@ -31,36 +16,40 @@ async function saveTiktokData(client, selectedKeywords, selectedCountries, perio
 // 	}
 // 	return Data;
 // }
+	const promises = [];
+
 	for (const country of selectedCountries) {
 		for (const keyword of selectedKeywords) {
 			console.log(`Country: ${country}, Keyword: ${keyword}`);
-			try {
-				const result = await client.tiktok.fullKeywordSearch({
+			promises.push(
+				client.tiktok.fullKeywordSearch({
 					keyword: keyword,
 					country: country,
 					period: Number(period), //주어진 기간보다 최근 게시물 = "0", "1", "7", "30", "90", "180" (일)
 					sorting: Number(sorting), // 0: 관련성, 1: 좋아요순
 					match_exactly: matchExactly, // 정확히 일치하는 게시물만 가져오기 (true/false)
-				});
-				console.log(result);
-				if (result.error) {
-					console.error(`API Error for country "${country}", keyword "${keyword}":`, result.error);
-					continue;
-				}
-				console.log(`Data Success for country "${country}", keyword "${keyword}" (${result.data.length} items)`);
+				}).then(result => {
+					if (result.error) {
+						console.error(`API Error for country "${country}", keyword "${keyword}":`, result.error);
+						return;
+					}
 
-				Data.push({
-					country: country,
-					keyword: keyword,
-					data: result.data,
+					console.log(`Data Success for country "${country}", keyword "${keyword}" (${result.data.length} items)`);
+
+					data.push({
+						country: country,
+						keyword: keyword,
+						data: result.data,
+					});
+				}).catch(error => {
+					console.error(`Exception for country "${country}", keyword "${keyword}":`, error);
 				})
-			}
-			catch (error) {
-				console.error(`Exception for country "${country}", keyword "${keyword}":`, error);
-			}
+			);
 		}
 	}
-	return Data;
+
+	await Promise.all(promises);
+	return data;
 }
 
 module.exports = {
